609e0ae5ed967d372aefae6de02bb15c9ce0c645
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-01 13:14:51 +0100

    Update TestpFinalApplicationTests.java

--- src/test/java/com/example/demo/TestpFinalApplicationTests.java
+++ src/test/java/com/example/demo/TestpFinalApplicationTests.java
@@ -1,7 +1,12 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.validation.ValidProduit;
 
 @SpringBootTest
 class TestpFinalApplicationTests {
@@ -9,5 +14,11 @@ class TestpFinalApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+@Test
+void verifachat()
+{
+ValidProduit vp=new ValidProduit();
+assertTrue(vp.validPrix(23, 26));
 
+}
 }
